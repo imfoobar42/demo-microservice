74ca51306f80044bda6e134bed28212b1b91101b
Singh <akash.m.singh@capgemini.com>
2019-05-08 14:24:58 +0530

    Microservice

---
+++ movie-rating-service/src/main/java/com/capgemini/movierating/MovieRatingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.movierating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan("com.capgemini")
+public class MovieRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingServiceApplication.class, args);
+	}
+
+}
